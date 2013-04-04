12b8a722ecf4d158f9d3605efd6d435ff48cfe77
Justin Martin <justin.d.martin1@gmail.com>
2013-04-04 11:22:27 -0500

    Added ISBN13 validation

--- src/validate.js
+++ src/validate.js
@@ -1,2 +1,26 @@
 var validate = {
+	ISBN13: function(ISBN){
+		var ISBNArray = ISBN.split('');
+		var ISBNCheckDigit = 0;
+		for(var i = 1; i < 13; i++){
+			ISBNArray[i-1] = parseInt(ISBNArray[i-1]);
+			if(i % 2 == 0){
+				ISBNCheckDigit += ISBNArray[i-1] * 3;
+			}else{
+				ISBNCheckDigit += ISBNArray[i-1];
+			}
+		}
+		ISBNArray[12] = parseInt(ISBNArray[12]);
+		ISBNCheckDigit = ISBNCheckDigit % 10;
+		if(ISBNCheckDigit != 0){
+			ISBNCheckDigit = 10 - ISBNCheckDigit;
+		}
+		
+		//is valid?
+		if(ISBNCheckDigit == ISBNArray[12]){
+			return true;
+		}else{
+			return false;
+		}
+	}
 };
