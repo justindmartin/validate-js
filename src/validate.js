c31293c891816c6e17317813a446590b12aa3c0e
Justin Martin <justin.d.martin1@gmail.com>
2013-04-05 00:54:58 -0500

    Added URL and Email Address validation, need to troubleshoot ISBN test cases

--- src/validate.js
+++ src/validate.js
@@ -1,8 +1,18 @@
 var validate = {
-	ISBN13: function(ISBN){
+	email: function(emailAddress){
+		var regex = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
+		return regex.test(emailAddress);
+	},
+	ISBN: function(ISBN){
+
+		/*if(ISBN.length != 10 && ISBN.length != 13){
+			return false;
+		}*/
+
 		var ISBNArray = ISBN.split('');
 		var ISBNCheckDigit = 0;
-		for(var i = 1; i < 13; i++){
+		console.log(ISBNArray[ISBNArray.length-1]);
+		for(var i = 1; i < ISBNArray.length; i++){
 			ISBNArray[i-1] = parseInt(ISBNArray[i-1]);
 			if(i % 2 == 0){
 				ISBNCheckDigit += ISBNArray[i-1] * 3;
@@ -10,17 +20,23 @@ var validate = {
 				ISBNCheckDigit += ISBNArray[i-1];
 			}
 		}
-		ISBNArray[12] = parseInt(ISBNArray[12]);
+		ISBNArray[ISBNArray.length-1] = parseInt(ISBNArray[ISBNArray.length-1]);
 		ISBNCheckDigit = ISBNCheckDigit % 10;
-		if(ISBNCheckDigit != 0){
+		if(ISBNCheckDigit != 0 && ISBNArray[ISBNArray.length-1] == 'X'){
+			ISBNCheckDigit = 'X';
+		}else if(ISBNCheckDigit != 0){
 			ISBNCheckDigit = 10 - ISBNCheckDigit;
 		}
 		
 		//is valid?
-		if(ISBNCheckDigit == ISBNArray[12]){
+		if(ISBNCheckDigit == ISBNArray[ISBNArray.length-1]){
 			return true;
 		}else{
 			return false;
 		}
+	},
+	URL: function(URL){
+		var regex = new RegExp(/^((ht|f)tps?\:\/\/)?[a-zA-Z]{1}([\w\-]+\.)+([\w]{2,5})/i);
+		return regex.test(URL);
 	}
 };
